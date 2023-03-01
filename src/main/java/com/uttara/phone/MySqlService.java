c84dc029e2e56315ea5890945169da15d5ec46e2
MarioJA <mariojoshua.ind@gmail.com>
2023-03-01 15:49:23 +0530

    Impl Delete statements

--- src/main/java/com/uttara/phone/MySqlService.java
+++ src/main/java/com/uttara/phone/MySqlService.java
@@ -54,8 +54,7 @@ public class MySqlService extends IOService {
               
     }
 
-    public static void rollbackSQLCommit(Connection connection)
-	{
+    public static void rollbackSQLCommit(Connection connection) {
 		if(connection !=null)
 			try {
 				connection.rollback();
@@ -73,7 +72,75 @@ public class MySqlService extends IOService {
     public Boolean writeContacts(ContactBean contactBean, String phoneBookName) {
         try (Connection connection = getConnection()) {
             connection.setAutoCommit(false); 
-            ps1 = connection.prepareStatement(
+            int contacts_ID = insertIntoContactsTable(contactBean, phoneBookName);
+            int phoneNumber_ID = insertIntoPhoneNumberTable(contactBean, contacts_ID);
+            int email_ID = insertIntoEmailTable(contactBean,contacts_ID);
+            int tags_ID = insertIntoTagsTable(contactBean,contacts_ID);
+            connection.commit();
+            return true;
+        } catch (SQLException se) {
+            rollbackSQLCommit(connection);
+            se.printStackTrace();
+            return false;
+        }
+    }
+
+    private int insertIntoTagsTable(ContactBean contactBean, int contacts_ID) {
+        for (String tag: contactBean.tags()) {
+            ps2 = connection.prepareStatement(
+                """
+                INSERT INTO contactApp.tags
+                (tag)
+                VALUES(?,""" + Statement.RETURN_GENERATED_KEYS + ")");
+            ps2.setString(1, tag); 
+            ps2.executeQuery(); 
+
+            resultSet = ps2.getGeneratedKeys();
+            int tag_ID = resultSet.getInt("ID");
+        return 0;
+    }
+
+    private int insertIntoContactsTagsLinkTable(int tag_ID,  int contacts_ID) {
+        PreparedStatement ps3 = connection.prepareStatement
+        ("""
+        INSERT INTO contacts_tags
+        (contacts_ID, tag_ID)
+        VALUES(?,?)""");
+        ps3.setInt(1, contacts_ID);
+        ps3.setInt(2, tag_ID);
+        ps3.executeQuery();
+    }
+    }
+
+    private int insertIntoEmailTable(ContactBean contactBean, int contacts_ID) throws SQLException {
+        for (String emailid: contactBean.email()) {
+            ps2 = connection.prepareStatement(
+                """
+                INSERT INTO contactApp.email
+                (contacts_ID,emailid)
+                VALUES(""" + contacts_ID +",?)");
+            ps2.setString(2, emailid); 
+            ps2.executeQuery();  
+        }
+        return 0;
+    }
+
+    private int insertIntoPhoneNumberTable(ContactBean contactBean, int contacts_ID) throws SQLException {
+        for (String phoneNumber: contactBean.phoneNumbers()) {
+            ps2 = connection.prepareStatement(
+                """
+                INSERT INTO contactApp.phonenumber
+                (contacts_ID,phoneNumber)
+                VALUES(""" + contacts_ID +",?)");
+            ps2.setString(0, phoneNumber);
+            ps2.executeQuery();   
+        }
+        return 0;
+    }
+
+
+    public int insertIntoContactsTable(ContactBean contactBean, String phoneBookName) throws SQLException {
+        ps1 = connection.prepareStatement(
                 """
                 INSERT INTO contactApp.contacts\
                 (phonebook_ID, gender, fullname, petname, dateOfBirth, address)\
@@ -88,68 +155,8 @@ public class MySqlService extends IOService {
             ps1.executeQuery();
             resultSet = ps1.getGeneratedKeys();
             int contacts_ID = resultSet.getInt("ID");
-
-            for (String phoneNumber: contactBean.phoneNumbers()) {
-                ps2 = connection.prepareStatement(
-                    """
-                    INSERT INTO contactApp.phonenumber
-                    (contacts_ID,phoneNumber)
-                    VALUES(""" + contacts_ID +",?)");
-                ps2.setString(0, phoneNumber);
-                ps2.executeQuery();   
-            }
-
-            for (String emailid: contactBean.email()) {
-                ps2 = connection.prepareStatement(
-                    """
-                    INSERT INTO contactApp.email
-                    (contacts_ID,emailid)
-                    VALUES(""" + contacts_ID +",?)");
-                ps2.setString(2, emailid); 
-                ps2.executeQuery();  
-            }
-
-            for (String tag: contactBean.tags()) {
-                ps2 = connection.prepareStatement(
-                    """
-                    INSERT INTO contactApp.phonenumber
-                    (contacts_ID,phoneNumber)
-                    VALUES(""" + contacts_ID +",?)");
-                ps2.setString(0, tag); 
-                ps2.executeQuery();  
-            }
-
-            for (String tag: contactBean.tags()) {
-                ps2 = connection.prepareStatement(
-                    """
-                    INSERT INTO contactApp.tags
-                    (tag)
-                    VALUES(?,""" + Statement.RETURN_GENERATED_KEYS + ")");
-                ps2.setString(1, tag); 
-                ps2.executeQuery(); 
-
-                resultSet = ps2.getGeneratedKeys();
-                int tag_ID = resultSet.getInt("ID");
-                PreparedStatement ps3 = connection.prepareStatement
-                ("""
-                INSERT INTO contacts_tags
-                (contacts_ID, tag_ID)
-                VALUES(?,?)""");
-                ps3.setInt(1, contacts_ID);
-                ps3.setInt(2, tag_ID);
-                ps3.executeQuery();
-            }
-
-
-            connection.commit();
-            return true;
-        } catch (SQLException se) {
-            rollbackSQLCommit(connection);
-            se.printStackTrace();
-            return false;
-        }
+            return contacts_ID;
     }
-
 
     @Override
     public Boolean updateContacts(ContactBean contactBean) {
@@ -159,18 +166,38 @@ public class MySqlService extends IOService {
 
     @Override
     public Boolean deleteContact(String fullName, String phoneBookName) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-    @Override
-    public Boolean deleteContactBook(String phoneBookName) {
         try (Connection connection = getConnection()){
             connection.setAutoCommit(false);
             ps1 = connection.prepareStatement(
             """
-            INSERT INTO contactApp.phonebook_master 
-            (name) 
-            VALUES(?)""");
+            DELETE FROM contactApp.contacts
+            WHERE phoneBook_ID = 
+                (SELECT ID 
+                 FROM phonebook_master 
+                 WHERE name='"""+ phoneBookName+ "')" 
+              + "AND fullname=" + fullName + ";");
+            int rowsAffected = ps1.executeUpdate();
+            System.out.println(rowsAffected + " row(s) has been deleted!");
+            connection.commit();
+            return true;
+        } catch (SQLException e) {
+            rollbackSQLCommit(connection);    
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+
+    @Override
+    public Boolean deleteContactBook(String phoneBookName) {
+        try (Connection connection = getConnection()){
+            // Give warning that all contacts will also be deleted, delete all contacts first and then delte the contact book
+            deleteContact("IS NOT NULL", phoneBookName);
+            connection.setAutoCommit(false);
+            ps1 = connection.prepareStatement(
+            """
+            DELETE FROM contactApp.phonebook_master 
+            WHERE name(?)""");
             ps1.setString(1, phoneBookName.toUpperCase());
             ps1.execute();
             connection.commit();
