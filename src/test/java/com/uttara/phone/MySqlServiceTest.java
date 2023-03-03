e05be25b5279ba332e9d63f44513e39f90932bd2
MarioJA <mariojoshua.ind@gmail.com>
2023-03-03 19:43:35 +0530

    Added Junit tests

---
+++ src/test/java/com/uttara/phone/MySqlServiceTest.java
@@ -0,0 +1,52 @@
+package test.java.com.uttara.phone;
+
+import org.junit.Test;
+
+public class MySqlServiceTest {
+    @Test
+    public void testCreateContactBook() {
+        System.out.println("This test ran");
+    }
+
+    @Test
+    public void testContactBookExists() {
+
+    }
+
+    @Test
+    public void testContactExists() {
+
+    }
+
+
+
+    @Test
+    public void testDeleteContact() {
+
+    }
+
+    @Test
+    public void testDeleteContactBook() {
+
+    }
+
+    @Test
+    public void testInsertIntoContactsTable() {
+
+    }
+
+    @Test
+    public void testReadContact() {
+
+    }
+
+    @Test
+    public void testUpdateContacts() {
+
+    }
+
+    @Test
+    public void testWriteContacts() {
+
+    }
+}
